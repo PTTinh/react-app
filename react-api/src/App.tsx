cddc657f6c82d839e25947c7c48aad3253ffff1a
Phạm Trọng Tính <47069678+PTTinh@users.noreply.github.com>
2025-11-17 14:59:26 +0000

    update

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './assets/style.scss'
+import axios from 'axios'
+
+async function getRandomCallUrl() {
+  const apiKey = ''
+  const res = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key='+ apiKey)
+  return res.data[0].url;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [url, setUrl] = useState('');
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={async () => setUrl(await getRandomCallUrl())}>Click để lấy ảnh</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {url && <img src={url} alt="Ảnh"/>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
