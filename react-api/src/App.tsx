d175dfc785e913091a3458553bb8ee0b4aca95f0
Phạm Trọng Tính <47069678+PTTinh@users.noreply.github.com>
2025-11-19 14:48:19 +0000

    update

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -5,21 +5,31 @@ import axios from 'axios'
 
 async function getRandomCallUrl() {
   const apiKey = ''
-  const res = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key='+ apiKey)
+  const res = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=' + apiKey)
   return res.data[0].url;
 }
 
 function App() {
 
   const [url, setUrl] = useState('');
-
+  const [isLoading, setIsLoading] = useState(false);
+  async function Loading() {
+    setIsLoading(true)
+    setUrl(await getRandomCallUrl())
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }
   return (
     <>
+      {isLoading && <div className="loading">
+        Đang tải...
+      </div>}
       <div>
-        <button onClick={async () => setUrl(await getRandomCallUrl())}>Click để lấy ảnh</button>
+        <button onClick={Loading}>Click để lấy ảnh</button>
       </div>
       <div>
-        {url && <img src={url} alt="Ảnh"/>}
+        {url && <img src={url} alt="Ảnh" />}
       </div>
     </>
   )
